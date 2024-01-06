9ae5a74f7472c13684f99f94599a4a50dce79840
NSR <65774401+JeremyEngram@users.noreply.github.com>
2024-01-05 20:21:57 -0500

    Update pipmanage.py

--- pipmanage.py
+++ pipmanage.py
@@ -1,13 +1,14 @@
 import subprocess
 import os
 
-# Function to create and activate a virtual environment
+# Function to create a virtual environment
 def create_venv(venv_name):
-    subprocess.run([f'python3 -m venv {venv_name}'], shell=True)
+    subprocess.run(['python3', '-m', 'venv', venv_name])
 
 # Function to install package in the virtual environment
 def install_package(venv_name, package):
-    subprocess.run([f'{venv_name}/bin/pip install {package}'], shell=True)
+    pip_path = os.path.join(venv_name, 'bin', 'pip')
+    subprocess.run([pip_path, 'install', package])
 
 # Check if requirements.txt exists
 if not os.path.exists('requirements.txt'):
@@ -19,7 +20,9 @@ with open('requirements.txt', 'r') as file:
     for line in file:
         package = line.strip()
         if package:
-            venv_name = f'venv_{package}'
+            # Format the virtual environment directory name
+            package_name = package.split('==')[0]
+            venv_name = f'venv_{package_name}'
             print(f'Creating virtual environment in {venv_name} for package {package}...')
             create_venv(venv_name)
             install_package(venv_name, package)
