68235659da1b405b37ef623deac68d430f997744
NSR <65774401+JeremyEngram@users.noreply.github.com>
2024-01-05 19:46:16 -0500

    Create createvenvpips.py

---
+++ createvenvpips.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+import sys
+
+def create_and_activate_venv(venv_name):
+    # Create a virtual environment
+    subprocess.run([sys.executable, "-m", "venv", venv_name])
+
+    # Activate the virtual environment
+    # On Windows
+    if os.name == "nt":
+        activate_script = os.path.join(venv_name, "Scripts", "activate")
+    # On Unix or MacOS
+    else:
+        activate_script = os.path.join(venv_name, "bin", "activate")
+    
+    return activate_script
+
+def install_packages_from_file(file_name, venv_name):
+    # Activate the virtual environment
+    activate_script = create_and_activate_venv(venv_name)
+
+    # Install packages from the file
+    with open(file_name, "r") as file:
+        for line in file:
+            package = line.strip()
+            if package:
+                subprocess.run(f"{activate_script} && pip install {package}", shell=True)
+
+venv_name = "myenv"  # Name of the virtual environment
+file_name = "frozen_packages.txt"  # File containing the list of packages
+
+install_packages_from_file(file_name, venv_name)
